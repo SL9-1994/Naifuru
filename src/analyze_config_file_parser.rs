b28cac8a525c8b9a3128ec1a62f2c6a741ce5f59
yurari <320sl91994@gmail.com>
2024-12-18 23:21:39 +0900

    add:✨ Refactor configuration structures in analyze_config_file_parser. Updated Conversion struct to use enums for 'from' and 'to' fields, enhancing clarity. Introduced NameFormat enum for output file naming conventions. Improved read_config_from_input_file function to return a vector of errors, allowing for better error handling during configuration loading.

--- src/analyze_config_file_parser.rs
+++ src/analyze_config_file_parser.rs
@@ -6,16 +6,38 @@ use crate::errors::{ConfigParseError, CustomIoError};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Conversion {
-    from: String,
-    to: String,
+    /// Before conversion
+    from: ConversionFrom,
+    /// After conversion
+    to: ConversionTo,
+    /// File configuration groups
     groups: Vec<Group>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ConversionFrom {
+    JpNiedKnet,
+    UsScsnV2,
+    NzGeonetV1a,
+    NzGeonetV2a,
+    TwPalertSac,
+    TkAfadAsc,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum ConversionTo {
+    JpJmaCsv,
+    JpStera3dTxt,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Group {
     pub path: String,
+    /// direction component identifier(ns, ew, ud)
     pub component: Option<String>,
-    #[serde(default)]
+    /// identifier key for grouping
     pub g_key: Option<u32>,
 }
 
@@ -33,27 +55,37 @@ pub struct GlobalConfig {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GlobalSettings {
     /// output file name format
-    converted_name: String,
+    name_format: NameFormat,
     /// acceleration calculate option bool
     acc_calculate: bool,
     /// unit conversion option bool
     unit_conversion: bool,
 }
 
-impl Config {
-    pub fn read_config_from_input_file(
-        &self,
-        input_file_path: &Path,
-    ) -> Result<String, ConfigParseError> {
-        match fs::read_to_string(input_file_path) {
-            Ok(content) => Ok(content),
-            Err(e) => Err(ConfigParseError::Io(CustomIoError::from((
-                e,
-                input_file_path.to_path_buf(),
-            )))),
-        }
-    }
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+pub enum NameFormat {
+    /// ## **Example: 20240101-161018-ISK005-knet.csv**
+    /// - yyyymmdd:  Date and time of observation start date and time
+    /// - hhmmss: Hour, minute, second of the observation start date and time
+    /// - sn: Observation station name(ISK005, WVAS, etc...)
+    /// - n: Institution name(knet, geonet, etc...)
+    YyyymmddHhmmssSnN,
+}
 
+pub fn read_config_from_input_file(
+    input_file_path: &Path,
+) -> Result<String, Vec<ConfigParseError>> {
+    match fs::read_to_string(input_file_path) {
+        Ok(content) => Ok(content),
+        Err(e) => Err(vec![ConfigParseError::Io(CustomIoError::from((
+            e,
+            input_file_path.to_path_buf(),
+        )))]),
+    }
+}
+
+impl Config {
     /// Groups the `Group` objects within each `Conversion` by their `g_key` value.
     pub fn group_by_key(&self) -> Vec<Vec<&Group>> {
         let mut result: Vec<Vec<&Group>> = Vec::new();
