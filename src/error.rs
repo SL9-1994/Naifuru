f161c1c7ab2538cba87e562ab3ab63927409e79e
yurari <320sl91994@gmail.com>
2025-01-05 20:59:36 +0900

    feat:✨ ErrorContext構造体とModule列挙型を削除し、thiserrorでのエラー定義に変更

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,7 @@
 /// This module defines custom error types and utilities for handling errors in the application.
-use std::fmt;
+use std::{fmt, path::PathBuf};
+
+use thiserror::Error;
 
 #[macro_export]
 macro_rules! bail_on_error {
@@ -8,29 +10,62 @@ macro_rules! bail_on_error {
     }};
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Module {
-    CliArgsValidation,
-    ConfigFileAnalysis,
+// トップレベルカスタムエラー
+#[non_exhaustive]
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum AppError {
+    #[error("CLI error> {0}")]
+    Cli(#[from] CliErr),
+    // TODO: AnalysisConfigErr
 }
 
-impl Module {
-    pub fn exit_code(&self) -> i32 {
-        match self {
-            Module::CliArgsValidation => 2,
-            Module::ConfigFileAnalysis => 78, // EX_CONFIG
-        }
-    }
+#[non_exhaustive]
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum CliErr {
+    #[error("Args validation error> {0}")]
+    ArgsValidation(#[from] ArgsValidationErr),
 }
 
+#[non_exhaustive]
+#[derive(Error, Debug, PartialEq, Eq)]
+pub enum ArgsValidationErr {
+    #[error("File has no extension: {0}")]
+    NoExtension(PathBuf),
+    #[error("Invalid file extension: {0}, expected one of: {1}")]
+    InvalidExtension(String, String),
+    #[error("Path does not exist: {0}")]
+    PathDoesNotExist(PathBuf),
+    #[error("Path is not a file: {0}")]
+    PathIsNotFile(PathBuf),
+    #[error("Path is not a directory: {0}")]
+    PathIsNotDirectory(PathBuf),
+}
+
+// PartialEq, Eqの実装を行うための、std::io::ErrorをラップするカスタムI/Oエラー型
 #[derive(Debug)]
-pub struct ErrorContext {
-    pub message: String,
-    pub module: Module,
-}
+pub struct IoErrWrapper(pub std::io::Error);
 
-impl fmt::Display for ErrorContext {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.message)
+impl From<std::io::Error> for IoErrWrapper {
+    fn from(err: std::io::Error) -> Self {
+        IoErrWrapper(err)
     }
 }
+
+// IoErrWrapperにPartialEqを実装
+impl PartialEq for IoErrWrapper {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.kind() == other.0.kind() && self.0.to_string() == other.0.to_string()
+    }
+}
+
+// IoErrWrapperにEqを実装
+impl Eq for IoErrWrapper {}
+
+impl std::fmt::Display for IoErrWrapper {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "I/O error> {}", self.0)
+    }
+}
+
+// IoErrWrapperにErrorトレイトを実装（エラーをそのまま扱えるようにする）
+impl std::error::Error for IoErrWrapper {}
