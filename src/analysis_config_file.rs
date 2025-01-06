a503931f94c4d881fd44b5efd646095c0e42fca1
yurari <320sl91994@gmail.com>
2025-01-06 16:21:23 +0900

    feat:✨ Config, ConversionConfig, GroupConfig, FileConfigにvalidateメソッドを追加 - ConversionConfig構造体にassign_ext_based_on_fromメソッドを追加 - FileConfig構造体にvalidate_extension_for_acceptable_extsを追加 - ConversionConfig構造体とGroupConfig構造体からidフィールドを削除

--- src/analysis_config_file.rs
+++ src/analysis_config_file.rs
@@ -2,7 +2,7 @@ use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
 
-use crate::error::IoErrWrapper;
+use crate::error::{AnalysisConfigErr, AppError, ConfigValidationErr, IoErrWrapper};
 
 /// File format before conversion.  
 #[derive(Debug, Serialize, Deserialize)]
@@ -50,6 +50,25 @@ pub struct Config {
     pub conversion: Vec<ConversionConfig>,
 }
 
+impl Config {
+    pub fn validate(&self) -> Result<(), Vec<AppError>> {
+        let mut errors: Vec<AppError> = Vec::new();
+
+        for conv_config in &self.conversion {
+            let _ = conv_config.validate().map_err(|e| {
+                errors.extend(e.into_iter().map(AppError::from));
+            });
+        }
+
+        if !errors.is_empty() {
+            return Err(errors);
+        }
+
+        Ok(())
+    }
+}
+
+// MEMO: 列挙型はtomlによってバリデーションが行われるため、この構造体でバリデーション実装は行いません。
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GlobalConfig {
     pub name_format: NameFormat,
@@ -57,22 +76,123 @@ pub struct GlobalConfig {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ConversionConfig {
-    pub id: u16,
     pub from: From,
     pub to: To,
     pub group: Vec<GroupConfig>,
 }
 
+impl ConversionConfig {
+    pub fn validate(&self) -> Result<(), Vec<AnalysisConfigErr>> {
+        let mut errors: Vec<AnalysisConfigErr> = Vec::new();
+
+        for group_config in &self.group {
+            let acceptable_exts: &[&str] = Self::assign_ext_based_on_from(&self.from);
+            let _ = group_config.validate(acceptable_exts).map_err(|e| {
+                errors.extend(e.into_iter().map(AnalysisConfigErr::from));
+            });
+        }
+
+        if !errors.is_empty() {
+            return Err(errors);
+        }
+
+        Ok(())
+    }
+
+    fn assign_ext_based_on_from(from: &From) -> &[&str] {
+        match from {
+            From::JpNiedKnet => &["ns", "ew", "ud"],
+            From::UsScsnV2 => &["v2"],
+            From::NzGeonetV1a => &["v1a"],
+            From::NzGeonetV2a => &["v2a"],
+            From::TwPalertSac => &["sac"],
+            From::TkAfadAsc => &["asc"],
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GroupConfig {
-    pub id: u16,
     pub files: Vec<FileConfig>,
+}
+
+impl GroupConfig {
+    pub fn validate(&self, acceptable_exts: &[&str]) -> Result<(), Vec<AnalysisConfigErr>> {
+        let mut errors: Vec<AnalysisConfigErr> = Vec::new();
+
+        for file in &self.files {
+            let _ = file.validate(acceptable_exts).map_err(|e| {
+                errors.extend(e.into_iter().map(AnalysisConfigErr::from));
+            });
+        }
+
+        if !errors.is_empty() {
+            return Err(errors);
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FileConfig {
     pub path: PathBuf,
     pub acc_axis: Option<AccAxis>,
+}
+
+impl FileConfig {
+    pub fn validate(&self, acceptable_exts: &[&str]) -> Result<(), Vec<AnalysisConfigErr>> {
+        let mut errors: Vec<AnalysisConfigErr> = Vec::new();
+
+        let _ = self
+            .validate_extension_for_acceptable_exts(acceptable_exts)
+            .map_err(|e| {
+                errors.push(e);
+            });
+
+        let _ = self.validate_path().map_err(|e| {
+            errors.push(e);
+        });
+
+        if !errors.is_empty() {
+            return Err(errors);
+        }
+
+        Ok(())
+    }
+
+    fn validate_path(&self) -> Result<(), AnalysisConfigErr> {
+        if !self.path.exists() {
+            return Err(ConfigValidationErr::PathDoesNotExist(self.path.to_path_buf()).into());
+        } else if !self.path.is_file() {
+            return Err(ConfigValidationErr::PathIsNotFile(self.path.to_path_buf()).into());
+        }
+
+        Ok(())
+    }
+
+    fn validate_extension_for_acceptable_exts(
+        &self,
+        acceptable_exts: &[&str],
+    ) -> Result<(), AnalysisConfigErr> {
+        if let Some(extension) = self
+            .path
+            .extension()
+            .map(|ext| ext.to_string_lossy().to_lowercase())
+        {
+            if !acceptable_exts.contains(&extension.as_str()) {
+                return Err(ConfigValidationErr::InvalidExtension(
+                    acceptable_exts.join(", "),
+                    extension,
+                )
+                .into());
+            }
+        } else {
+            return Err(ConfigValidationErr::NoExtension(self.path.to_path_buf()).into());
+        }
+
+        Ok(())
+    }
 }
 
 pub fn read_config_from_input_file(input_file_path: &Path) -> Result<String, IoErrWrapper> {
