f840863880f1ac82b5c543107510a1b0ec3d9a69
yurari <320sl91994@gmail.com>
2025-01-08 18:04:05 +0900

    feat:✨データ抽出の為の抽象化と抽出されたデータを保持する構造体の追加

--- src/extractor/mod.rs
+++ src/extractor/mod.rs
@@ -1,2 +1,43 @@
+use crate::error::AnalysisErr;
+
 pub mod jp_jma_csv;
 pub mod jp_stera3d_txt;
+
+pub trait Extractor {
+    fn extract(&self) -> Result<ExtractedData, AnalysisErr>;
+}
+
+pub enum ExtractedData {
+    JpStera3dTxt(JpStera3dTxtData),
+    JpJmaCsv(JpJmaCsvData),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct JpStera3dTxtData {
+    num_of_elements: i32,
+    acc_values: Acceleration,
+    common: CommonValue,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct JpJmaCsvData {
+    site_code: String,
+    lat: f64,
+    lon: f64,
+    unit_type: String,
+    initial_time: String,
+    common: CommonValue,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct CommonValue {
+    sampling_rate: f32,
+    acc_values: Acceleration,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Acceleration {
+    ns: Vec<f64>,
+    ew: Vec<f64>,
+    ud: Vec<f64>,
+}
